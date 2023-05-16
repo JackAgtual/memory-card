acd7ca580d5159b030ce0851c3d9518ed3f3a14a
Jack Agtual <jaagtual@gmail.com>
2023-05-15 20:56:47 -0700

    Styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
+import HeaderCSS from '../styles/Header.module.css'
+
 export default function Header(props) {
   const { currentScore, highScore, fetchImages } = props
 
   return (
-    <div>
-      <h1>Memory Card Game</h1>
-      <div>Select as many cards as possible without selecting the same card twice</div>
-      <div>Current score: {currentScore}</div>
-      <div>High score: {highScore}</div>
-      <button onClick={fetchImages}>Get new cards</button>
+    <div className={HeaderCSS.header}>
+      <div>
+        <h1 className={HeaderCSS.title}>Memory Card Game</h1>
+        <div>Select as many cards as possible without selecting the same card twice</div>
+      </div>
+      <div className={HeaderCSS.score}>
+        <div>Current score: {currentScore}</div>
+        <div>High score: {highScore}</div>
+        <button className={HeaderCSS.newCardsBtn} onClick={fetchImages}>
+          Get new cards
+        </button>
+      </div>
     </div>
   )
 }
