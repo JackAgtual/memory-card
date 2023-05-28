2e8e91efbc838eb88234a501880ce698aee6d1a9
Jack Agtual <jaagtual@gmail.com>
2023-05-28 08:43:21 -0700

    Converting Unsplash service to ts

--- src/services/unsplash/Unsplash.ts
+++ src/services/unsplash/Unsplash.ts
@@ -1,11 +1,13 @@
 import { ACCESS_KEY } from '../../ApiKeys'
 
 export default class Unsplash {
+  baseUrl: string
+
   constructor() {
     this.baseUrl = 'https://api.unsplash.com'
   }
 
-  async fetchImages(numCards) {
+  async fetchImages(numCards: number) {
     const url = `${this.baseUrl}/photos/random?client_id=${ACCESS_KEY}&orientation=squarish&count=${numCards}`
     // const url = 'http://localhost:3500/data'
     const response = await fetch(url)
