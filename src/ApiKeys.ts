dc5987217bdb81f1f9887f2c6f3474f7ae69a22d
Jack Agtual <jaagtual@gmail.com>
2023-05-28 13:58:38 -0700

    Converting ApiKeys to ts

---
+++ src/ApiKeys.ts
@@ -0,0 +1,4 @@
+// https://api.unsplash.com/photos/?client_id=s13VqUTywSAmCFyq-hTi8Yg0SGQ-GELQng3bqiowV64
+
+export const ACCESS_KEY: string = 's13VqUTywSAmCFyq-hTi8Yg0SGQ-GELQng3bqiowV64'
+export const SECRET_KEY: string = 'YREDOmKYg0epBDRYMjZ8awYCR5eyQLn-QQqxf-gaZ50'
